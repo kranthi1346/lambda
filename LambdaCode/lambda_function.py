08ef13442967add1167a9bcc59b9441ca1f2ec3a
kranthi1346 <46166536+kranthi1346@users.noreply.github.com>
2019-09-25 21:04:10 +0530

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,13 +1,7 @@
 import boto3
+import json
 
 ec2 = boto3.client('ec2')
-
-# Retrieves all regions/endpoints that work with EC2
-response = ec2.describe_regions()
-print('Regions:', response['Regions'])
-
-# Retrieves availability zones only for region of the ec2 object
-response = ec2.describe_availability_zones()
-print('Availability Zones:', response['AvailabilityZones'])
- 
- 
+def lambda_handler(event, context):
+    response = ec2.describe_availability_zones()
+    return {"statusCode": 200, "body": json.stringify(response)}
