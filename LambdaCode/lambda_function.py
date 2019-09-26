80a3ba6da4a8dd69aca7b327eb4b042b98db066b
kranthi1346 <46166536+kranthi1346@users.noreply.github.com>
2019-09-26 20:29:50 +0530

    Update lambda_function.py

--- LambdaCode/lambda_function.py
+++ LambdaCode/lambda_function.py
@@ -1,9 +1,8 @@
+  
 import boto3
 import json
-    def lambdaTest(event, context)
-        response = {}
-        dummybody = {'body':'something'}
-        response['statusCode'] = 200
-        response['body'] = dummybody
-        response["headers"] = {"Content-Type": "application/json"},
-        return json.dumps(response)
+
+ec2 = boto3.client('ec2')
+def lambda_handler(event, context):
+    response = ec2.describe_availability_zones()
+    return {"statusCode": 200, "body": json.dumps(response)}
